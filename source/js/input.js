f7a45d1664e788378fa7360947366a2409f826f1
AndrusovN <andrusov.n@gmail.com>
2020-11-22 00:38:26 +0300

    bugfix in tex parsing

--- source/js/input.js
+++ source/js/input.js
@@ -7,69 +7,50 @@ var keywords = {
     'tg': '\\tan',
 }
 
-function parseToCasual(formula){
+function parseToTeX(formula){
     formula = formula.replaceAll(' ', '')
 
-    var brackets = Array()
-    var toPop = Array()
-    var depth = Array()
-    var depthWasThere = Array()
-
-    console.log(formula)
-
-    for(var i = 0; i < formula.length; i++){
-        if(formula[i] == ')'){
-            toPop.push(2)
-        }
-        if(formula[i] == '/'){
-            formula = formula.substring(0, brackets[brackets.length - 1]) + '\\frac{' + 
-                formula.substring(brackets[brackets.length - 1], i) + '}{' + formula.substring(i+1, formula.length);
-            depth.push(brackets.length - 1)
-            depthWasThere.push(false)
-            i += 6
-        }
+    var match = formula.match('/')
+    while(match){
         
-        if(toPop.includes(1)){
-            brackets.pop()
-            console.log('aaaa')
-            console.log(i)
-            if(depth.includes( brackets.length )){
-                if(depthWasThere[depth.indexOf(brackets.length)]){
-                    var ind = /*formula[i] == ')' ? i-1 : */i;
-                    console.log(formula)
-                    console.log(i)
-                    console.log("there")
-                    formula = formula.substring(0, ind) + '}' + formula.substring(ind, formula.length)
-                    depth[depth.indexOf(brackets.length)] = -1
-                } else {
-                    depthWasThere[depth.indexOf(brackets.length)] = true
-                }
-                
+
+        var started1 = false
+        var count1 = 0
+
+        for(var i = match.index+1; i < formula.length; i++){
+            if(formula[i] == '('){
+                started1 = true
+                count1 += 1
             }
-            toPop[toPop.indexOf(1)] = -1
+            if(formula[i] == ')'){
+                count1 -= 1
+            }
+            if(count1 == 0 && started1){
+                break;
+            }
         }
-        if(toPop.includes(2)){
-            toPop[toPop.indexOf(2)] = 1
-        }
-        if(formula[i] == '('){
-            brackets.push(i)
-        }
-    }
-    console.log(formula)
 
-    if(toPop.includes(1)){
-        brackets.pop()
-        if(depth.includes( brackets.length)){
-            var ind = /*formula[formula.length - 1] == ')' ? formula.length - 1 : */formula.length;
-            console.log(formula)
-            console.log(i)
-            console.log("here")
-            formula = formula.substring(0, ind) + '}' + formula.substring(ind, formula.length)
-            depth[depth.indexOf(brackets.length)] = -1
-        }
-    }
+        var started2 = false
+        var count2 = 0
 
-    console.log(formula)
+        for(var j = match.index - 1; j > 0; j--){
+            if(formula[j] == ')'){
+                started2 = true
+                count2 += 1
+            }
+            if(formula[j] == '('){
+                count2 -= 1
+            }
+            if(count2 == 0 && started2){
+                break;
+            }
+        }
+
+        formula = formula.substring(0, j) + '\\frac{' + formula.substring(j+1, match.index-1) + '}{' 
+            + formula.substring(match.index + 2, i) + '}' + formula.substring(i+1, formula.length);
+
+        match = formula.match('/')
+    }
 
     formula = formula.replaceAll(')', ' \\right)}')
     formula = formula.replaceAll('(', '{\\left( ')
@@ -79,35 +60,34 @@ function parseToCasual(formula){
     for(var key in keywords){
         formula = formula.replaceAll(key, keywords[key])
     }
-
-    //var nFormula = formula
-
-
     console.log(formula)
 
     return formula
 }
 
-var app = new Vue({
-    el: '#app',
+var app1 = new Vue({
+    el: '#app1',
     data: {
         text: ""
     },
-    computed: {
-        texText(){
-            var textLaTeX = parseToCasual(this.text) 
+    methods: {
+        recalculate(){
+            var textLaTeX = parseToTeX(this.text) 
 
-            var a = document.getElementById(1)
+            var a = document.getElementById("LaTeX-formula")
             if(a){
                 a.remove()
             }
-
+            var b = document.getElementById("formula-view")
             var d = document.createElement("div")
             d.innerHTML = '<latex-js>\\documentclass{article}\n\\begin{document}\n$$ ' + textLaTeX + ' $$ \n\\end{document}</latex-js>'
-            d.id = 1
-            document.body.appendChild(d)
-
-            
+            d.id = "LaTeX-formula"
+            b.appendChild(d)
+        }
+    },
+    computed: {
+        inputText(){
+            this.recalculate();
             return ""
         }
     }
