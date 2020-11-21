6521b8368603b78fb5259e4475573677f9139630
Alexey <alexey.nebolsin.jr@gmail.com>
2020-11-22 01:02:03 +0300

    Bad try to add formula converter

--- source/js/Graph_draw.js
+++ source/js/Graph_draw.js
@@ -4,9 +4,14 @@ class point {
         this.y = Y;
     }
 }
-var points = [];
-for (var i = -5; i < 5; i += 0.01) {
-    points.push(new point(i, Math.sin(i) * 3))
+
+function gen_points(func, down, up) {
+    var data = new Array();
+    for (var i = down; i < up; i += 0.01) {
+        data.push(new point(i, func.calculate(3.5)));
+        console.log(i);
+    }
+    return data;
 }
 
 function Scale(pnt, minX, minY, maxX, maxY, plot) {
@@ -22,46 +27,47 @@ function mod(A, B) {
     return A - Math.floor(A / B);
 }
 
-function Redraw(pts, minX, minY, SZX, SZY) {
+function Redraw(func, minX, minY, SZX, SZY) {
     var cvas = document.getElementById("plot");
     var ctx = cvas.getContext("2d")
+    ctx.clearRect(0, 0, cvas.width, cvas.height);
     minY *= -1;
     var maxX = minX + SZX;
     var maxY = minY + SZY;
+    pts = gen_points(func, minX, maxX);
+    console.log("Points: " + pts);
     ctx.beginPath();
     ctx.lineWidth = 1;
     //ctx.strokeStyle = 'silver';
     ctx.font = "7px Verdana";
-    for (var i = 0; i < 1; i++) {
-        var step = Math.pow(10, i);
-        for (var XX = maxX + step - maxX % step; XX >= minX; XX -= step) {
-            ctx.beginPath()
-            if (XX == 0) {
-                ctx.strokeStyle = 'black';
-            } else {
-                ctx.strokeStyle = 'silver';
-            }
-            var pltx = cvas.width - 2;
-            var gX = (XX - minX) * pltx / (maxX - minX);
-            ctx.moveTo(gX + 1, 0);
-            ctx.fillText(Math.round(XX), gX + 2, cvas.height);
-            ctx.lineTo(gX + 1, cvas.height)
-            ctx.stroke()
+    var step = Math.pow(10, Math.max(Math.floor(Math.log10(SZX)), Math.floor(Math.log10(SZY))));
+    for (var XX = maxX + step - maxX % step; XX >= minX; XX -= step) {
+        ctx.beginPath()
+        if (XX == 0) {
+            ctx.strokeStyle = 'black';
+        } else {
+            ctx.strokeStyle = 'silver';
         }
-        for (var XX = maxY + step - maxY % step; XX >= minY; XX -= step) {
-            ctx.beginPath()
-            if (XX == 0) {
-                ctx.strokeStyle = 'black';
-            } else {
-                ctx.strokeStyle = 'silver';
-            }
-            var pltx = cvas.height - 2;
-            var gX = (XX - minY) * pltx / (maxY - minY);
-            ctx.moveTo(0, gX + 1);
-            ctx.fillText(-Math.round(XX), 2, gX);
-            ctx.lineTo(cvas.width, gX + 1)
-            ctx.stroke()
+        var pltx = cvas.width - 2;
+        var gX = (XX - minX) * pltx / (maxX - minX);
+        ctx.moveTo(gX + 1, 0);
+        ctx.fillText(Math.round(XX), gX + 2, cvas.height);
+        ctx.lineTo(gX + 1, cvas.height)
+        ctx.stroke()
+    }
+    for (var XX = maxY + step - maxY % step; XX >= minY; XX -= step) {
+        ctx.beginPath()
+        if (XX == 0) {
+            ctx.strokeStyle = 'black';
+        } else {
+            ctx.strokeStyle = 'silver';
         }
+        var pltx = cvas.height - 2;
+        var gX = (XX - minY) * pltx / (maxY - minY);
+        ctx.moveTo(0, gX + 1);
+        ctx.fillText(-Math.round(XX), 2, gX);
+        ctx.lineTo(cvas.width, gX + 1)
+        ctx.stroke()
     }
     ctx.beginPath();
     ctx.strokeStyle = 'orange';
@@ -74,5 +80,3 @@ function Redraw(pts, minX, minY, SZX, SZY) {
     }
     ctx.closePath();
 }
-
-Redraw(points, 0, 1, 1, 1);
